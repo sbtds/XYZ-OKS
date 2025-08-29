776e45d68b7936b8e059d20b357c6e5c16bdd26a
sbtds <shibata@direcsion.com>
2025-08-29 17:06:03 +0900

    usefulDetail

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -6,7 +6,7 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPl
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 // HTMLファイルのパターンを取得
-const htmlFiles = ["index.html", "consultant.html", "consultant_detail.html", "pickup.html", "pickup_detail.html", "recruit.html", "contact.html", "photo.html"];
+const htmlFiles = ["index.html", "consultant.html", "consultant_detail.html", "pickup.html", "pickup_detail.html", "useful.html", "useful_detail.html", "contact.html"];
 
 // 英語版HTMLファイル
 const enHtmlFiles = [];
